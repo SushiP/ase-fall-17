373152dc297dc9ca7e21e5fd83412f534d707ac5
Sushi <giulioaur@hotmail.com>
2017-09-27 11:16:39 +0200

    Added support for negative number.

--- Lab1/src/calculator/CalculatorPrototype.java
+++ Lab1/src/calculator/CalculatorPrototype.java
@@ -15,6 +15,10 @@ public class CalculatorPrototype implements CalculatorIF {
     public int sum(int m, int n) {
         int result = m;
         
+        //Check whenever or not the two operands have negative value. 
+        if(m > 0 && n < 0) return this.subtract(m, -n); // m -n.
+        else if(m < 0) return this.subtract(n, -m);     // n -m or -m -n.
+        
         for(int i = 0; i < n; i++) result++;
         
         return result;
@@ -22,6 +26,10 @@ public class CalculatorPrototype implements CalculatorIF {
 
     @Override
     public int subtract(int m, int n) {
+        //Check whenever or not the two operands have negative value. 
+        if(m > 0 && n < 0) return this.sum(m, -n);  //m - (-n) = m + n. 
+        else if(n < 0){ int a = -m; m = -n; n = a; }//(-m) - (-n) = n - m;
+        
         int result = m;
         
         for(int i = 0; i < n; i++) result--;
@@ -34,20 +42,32 @@ public class CalculatorPrototype implements CalculatorIF {
         //Check the input is right.
         if(n == 0) throw new ArithmeticException("You cannot divide by 0");
         
-        //Compute division using subtraction.
+        boolean changeSign = false;
         int result;
+        
+        //Check whenever or not the sign must be changed and make the value positive;
+        if(m < 0){ m = - m; changeSign ^= true; }
+        if(n < 0){ n = -n; changeSign ^= true; }
+        
+        //Compute division using subtraction.
         for(result = 0; m >= n; result++) m -= n;
-        return result;
+        
+        return changeSign ? -result : result;
     }
 
     @Override
     public int multiply(int m, int n) {
         int result = 0;
+        boolean changeSign = false;
         
-        for(int i = 0; i < m ; i++ )
-            result += n;
+        //Check whenever or not the sign must be changed and make the value positive;
+        if(m < 0){ m = - m; changeSign ^= true; }
+        if(n < 0){ n = -n; changeSign ^= true; }
+        
+        for(int i = 0; i < m ; i++ ) result += n;
       
-        return result;
+        //Return the result with right sign.
+        return changeSign ? -result : result;
     }
     
 }
